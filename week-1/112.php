04ddc4b4b70451c1964c2d880760b87aa560388f
sbiimrly <imodaysbim07@gmail.com>
2025-02-18 07:07:10 +0700

    [~] change text

--- week-1/112.php
+++ week-1/112.php
@@ -1,20 +1,43 @@
 <html>
-    <head>
-        <title>
-            イモ
-        </title>
-    </head>
-    <body>
-        <h1>112.php</h1>
-        <p>
-            <?php
-            echo "perkenalkan saya Sang Bimo!";
-            echo '<br/ > Saya sedang bersekolah di SMKN 8 Semarang';
+<head>
+    <title>
+        Imo Belajar PHP
+    </title>
 
-            $angka = 112;
-            $nama = "Bimo";
-            echo '<br/ > Saya angkatan tahun 20'.$angka;
-            ?>
-    </p>
-    </body>
+    <style>
+        .biru {
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+    Hi, Perkenalkan
+    <?php
+    $walkes = 'Bu Yuniar';
+
+    echo "<br> Aku imo";
+    echo "<br> Aku sekolah di SNAPAN";
+    echo "<div class='biru'>Wali Kelas: ". $walkes ."</div>";
+
+    //ini komentar
+    #ini juga komentar
+    /*komentar*/
+
+    $angka1 = 5;
+    $angka2 = 10;
+    $penjumlahan = $angka1 + $angka2;
+    $x = $angka1 * $angka2;
+
+    echo "<br> $angka1 + $angka2 = " . $penjumlahan;
+    echo "<br>";
+    echo $x ." = $angka1 x $angka2";
+    
+    echo "<br> $angka1<sup>$angka2</sup> = ";
+    echo number_format (pow($angka1, $angka2), 2, ',','.');
+    /*number_format(angkanya,
+    jumlah angka di belakang koma,
+    'tanda koma/titik paling belakang')'*/
+?>
+
+</body>
 </html>
