3472c608b6eba23e762aa09f92f900b04130b7cf
sbiimrly <imodaysbim07@gmail.com>
2025-02-18 07:04:09 +0700

    [+] first init

---
+++ week-1/112.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>
+            イモ
+        </title>
+    </head>
+    <body>
+        <h1>112.php</h1>
+        <p>
+            <?php
+            echo "perkenalkan saya Sang Bimo!";
+            echo '<br/ > Saya sedang bersekolah di SMKN 8 Semarang';
+
+            $angka = 112;
+            $nama = "Bimo";
+            echo '<br/ > Saya angkatan tahun 20'.$angka;
+            ?>
+    </p>
+    </body>
+</html>
